32e6733b929e84a54f875164a16cf686379b8194
freehug071 <freehug071@gmail.com>
2022-12-08 15:28:26 -0800

    mube

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
 /**
-** main - Entry point
-**
-** Return: Always 0 (Success)
-**/
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
