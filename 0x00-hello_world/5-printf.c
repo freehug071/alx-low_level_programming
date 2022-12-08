f82e9983ff4a8dd9975480d02e13735936fae48f
freehug071 <freehug071@gmail.com>
2022-12-08 15:21:07 -0800

    0. Preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #!/bin/bash
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+** main - Entry point
+**
+** Return: Always 0 (Success)
+**/
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
