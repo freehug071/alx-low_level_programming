f5237569835fc552ef45ce784a48dd3d1d7c1b27
freehug071 <freehug071@gmail.com>
2022-12-12 00:15:33 -0800

    mbe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry
- * return 1
+ * main - prints exactly "and that piece of arts is useful"
+ * - Dora korpar, 2015-10-19
+ *   followed by a new line, to the standard error.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"- Dora korpor, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
